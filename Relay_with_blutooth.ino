47f8d6b8cae3bbdc48481cb33d6c5c04fb7b5bcd
muhammadshahab5 <77330005+muhammadshahab5@users.noreply.github.com>
2021-02-18 19:00:06 +0500

    Add files via upload

---
+++ Relay_with_blutooth.ino
@@ -0,0 +1,20 @@
+byte val;
+void setup ()
+{
+   Serial.begin(9600);
+   pinMode (2, OUTPUT);
+  
+}
+void loop()
+{
+  int a=0;
+  if(Serial.available())
+{
+  val=Serial.read();
+  Serial.println(int(val));
+  if(int(val)==49)
+     digitalWrite (2, HIGH);
+  else if (int(val)==50)
+      digitalWrite (2, LOW);
+}
+}
